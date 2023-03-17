59c95c23881f8e387a9749d270e8976d55d63a69
Vinicius203 <viniciusmartinsign@gmail.com>
2023-03-17 19:16:44 -0300

    Classe - Conta Bancária

---
+++ POO/conta.py
@@ -0,0 +1,16 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print(f'O saldo de {self.titular} é: {self.saldo}!')
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
