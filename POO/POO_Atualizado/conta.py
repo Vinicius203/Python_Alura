122b533122f6d6e655382c8fad829e625e49e463
Vinicius203 <viniciusmartinsign@gmail.com>
2023-07-05 12:01:25 -0300

    Código Principal - Conta

--- POO/POO_Atualizado/conta.py
+++ POO/POO_Atualizado/conta.py
@@ -5,8 +5,27 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print(f"O saldo do titular {self.titular} é: R$ {self.saldo:.2f}")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+        else:
+            print(
+                "O valor de saque é maior do que o saldo!"
+                " A operação não foi realizada."
+            )
+
 
 conta = Conta(123, "Vinicius", 15.70, 1130)
+conta2 = Conta(210, "Aragorn", 761.92, 3200)
 
-conta.titular
-print("x")
+conta.extrato()
+conta.deposita(4.30)
+conta.extrato()
+conta.saca(20.01)
+conta.extrato()
