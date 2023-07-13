20020ef96d4bf1408f3347554d14eade4390f054
Vinicius203 <viniciusmartinsign@gmail.com>
2023-07-13 11:14:01 -0300

    chama funções

---
+++ brasilidades/main.py
@@ -0,0 +1,12 @@
+from cpf_cnpj import CPF_CNPJ
+from validate_docbr import CNPJ
+
+# cpf_um = Cpf("15316264754")
+# print(cpf_um)
+
+exemplo_cnpj = "35379838000112"
+
+# cnpj = CNPJ()
+# print(cnpj.validate_exemplo_cnpj))
+
+documento = CPF_CNPJ(exemplo_cnpj, "cnpj")
