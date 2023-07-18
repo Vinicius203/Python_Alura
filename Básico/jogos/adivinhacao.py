191fa83775ba7b700ec8fbce8d9de0d9c7bb6f0c
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-07-18 10:59:18 -0300

    .

--- Básico/jogos/adivinhacao.py
+++ Básico/jogos/adivinhacao.py
@@ -6,15 +6,15 @@ import random
 
 
 def jogar():
-    print('********************************')
-    print('Bem vindo ao jogo de Adivinhação')
-    print('********************************')
+    print("********************************")
+    print("Bem vindo ao jogo de Adivinhação")
+    print("********************************")
 
     numero_secreto = random.randint(1, 100)
     print(numero_secreto)
 
-    print('Em qual nível de dificuldade você quer jogar?')
-    print('(1) Fácil (2) Médio (3) Difícil')
+    print("Em qual nível de dificuldade você quer jogar?")
+    print("(1) Fácil (2) Médio (3) Difícil")
     pontos = 1000
 
     nivel = int(input())
@@ -27,14 +27,14 @@ def jogar():
         tentativas = 5
 
     for rodada in range(1, tentativas + 1):
-        print(f'Tentativa {rodada} de {tentativas}:')
+        print(f"Tentativa {rodada} de {tentativas}:")
 
         chute_str = input("Digite um número entre 1 e 100: ")
         chute = int(chute_str)
-        print(f'Você digitou o número: {chute}')
+        print(f"Você digitou o número: {chute}")
 
-        if (chute < 1 or chute > 100):
-            print('Você deve digitar um número entre 1 e 100!')
+        if chute < 1 or chute > 100:
+            print("Você deve digitar um número entre 1 e 100!")
             continue
 
         acertou = chute == numero_secreto
@@ -42,22 +42,20 @@ def jogar():
         menor = not maior
 
         if acertou:
-            print(
-                f'PARABÉNS! Você acertou! A sua pontuação foi de {pontos} pontos.')
+            print(f"PARABÉNS! Você acertou! A sua pontuação foi de {pontos} pontos.")
             break
         else:
             if maior:
-                print('Você errou! O seu chute foi maior que o número secreto!')
+                print("Você errou! O seu chute foi maior que o número secreto!")
             elif menor:
-                print('Você errou! O seu chute foi menor que o número secreto!')
+                print("Você errou! O seu chute foi menor que o número secreto!")
 
             pontos = pontos - abs(chute - numero_secreto)
 
-    print('Fim de jogo!')
+    print("Fim de jogo!")
     if tentativas == rodada:
-        print(
-            f'O número secreto era {numero_secreto}, você fez {pontos} pontos.')
+        print(f"O número secreto era {numero_secreto}, você fez {pontos} pontos.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     jogar()
-    
