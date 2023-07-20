5cd7c8e178d758dcf4f4478df903b9dcb065c0a9
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-07-20 12:04:50 -0300

    main code - chamada de funções e classes

--- brasilidades/main.py
+++ brasilidades/main.py
@@ -64,5 +64,9 @@ print(hoje.tempo_cadastro())
 import requests
 from acesso_cep import BuscaEndereco
 
-r = requests.get("https://viacep.com.br/ws/37890000/json/")
-print(r.text)
+cep = "01001000"
+objeto_cep = BuscaEndereco(cep)
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+
+print(f"{bairro}, {cidade} - {uf}")
