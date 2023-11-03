2ff4e4d9495aba86f5312d3829f898eeaf6d52dc
Vinicius Martins <viniciusmartinsign@gmail.com>
2023-11-03 18:00:52 -0300

    .

---
+++ POO_2/testes/main.py
@@ -0,0 +1,9 @@
+from bytebank import Funcionario
+
+
+def teste_idade():
+    funcionario_teste = Funcionario("Teste", "05/09/2001", 1111)
+    print(f"Teste = {funcionario_teste.idade()}")
+
+
+teste_idade()
