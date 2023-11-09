4659bcaf8401e781bfc620f0ecc1ec0d01af2a38
Vinicius203 <viniciusmartinsign@gmail.com>
2023-11-09 18:58:50 -0300

    refactor: function decrescimo_salario split in two

--- POO_2/bytebank-tdd/codigo/bytebank.py
+++ POO_2/bytebank-tdd/codigo/bytebank.py
@@ -26,7 +26,7 @@ class Funcionario:
         nome_quebrado = nome_completo.split(" ")
         return nome_quebrado[-1]
 
-    def decrescimo_salario(self):
+    def _eh_socio(self):
         sobrenomes = [
             "Bragança",
             "Windsor",
@@ -37,14 +37,17 @@ class Funcionario:
             "Tudor",
             "Ptolomeu",
         ]
-        if self._salario >= 100000 and (self.sobrenome() in sobrenomes):
+        return self._salario >= 100000 and (self.sobrenome() in sobrenomes)
+
+    def decrescimo_salario(self):
+        if self._eh_socio():
             decrescimo = self._salario * 0.1
             self._salario = self._salario - decrescimo
 
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception("O salário é muito alto para receber um bônus")
         return valor
 
     def __str__(self):
